f56d9c437c2e5e11f64fc6abd9854e8496976fde
TheMysteryz <themysteryz@users.noreply.github.com>
2024-02-12 03:41:55 +0100

    add contact me

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,10 @@ export const calculateAge = (birthdate: string) => {
 
 	return Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
 };
+
+export const strCapitalize = (str?: string) =>
+	(str && str[0]?.toUpperCase() + str.slice(1)) || '';
+
+export const strUppercase = (str?: string) => str?.toUpperCase() || '';
+
+export const strLowercase = (str?: string) => str?.toLowerCase() || '';
