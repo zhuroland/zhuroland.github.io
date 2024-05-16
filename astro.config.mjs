f758a882633fcbd27bd98da9b403eee645dd753f
TheMysteryz <themysteryz@users.noreply.github.com>
2024-05-16 19:25:44 +0200

    update portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,45 @@
 import { defineConfig } from 'astro/config';
-
-import mdx from "@astrojs/mdx";
+import starlight from '@astrojs/starlight';
+import conf from './config.json';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zhuroland.github.io',
-  integrations: [mdx()]
+	integrations: [
+		starlight({
+			title: `${conf.infos.author} Protfolio`,
+			social: conf.infos.socials,
+			sidebar: [
+				{
+					label: 'About Me',
+					link: '/about',
+				},
+				{
+					label: 'Projects',
+					items: [
+						{
+							label: 'My personal projects',
+							link: '/projects/personal',
+						},
+						{
+							label: 'My group projects',
+							link: '/projects/group',
+						},
+						{
+							label: 'My School Projects',
+							link: '/projects/school',
+						},
+					],
+				},
+				{
+					label: 'Contact Me',
+					link: '/contact',
+				},
+				{
+					label: 'Documentation',
+					autogenerate: { directory: 'documentation' },
+				},
+			],
+		}),
+	],
 });
--- src/types/media.ts
+++
@@ -1,1 +0,0 @@
-export type Media = 'github' | 'youtube' | 'linkedin' | "twitch";
