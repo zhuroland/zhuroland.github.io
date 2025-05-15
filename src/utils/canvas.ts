306990ab21029a11c1e1ed816b5eaac5733f5fb3
TheMysteryz <themysteryz@users.noreply.github.com>
2025-05-15 15:01:03 +0200

    use function instead of class

--- src/utils/canvas.ts
+++ src/utils/canvas.ts
@@ -1,28 +1,19 @@
-interface GameCanvas {
-    canvas: HTMLCanvasElement;
-    ctx: CanvasRenderingContext2D;
+export function getCanvasContext() {
+  const canvas = document.querySelector("[data-canvas-game]");
+  if (!canvas) {
+    throw new Error("Canvas element not found");
   }
-  class GameCanvas implements GameCanvas {
-    constructor() {
-      const canvas = document.querySelector("[data-canvas-game]");
-      if (!canvas) {
-        throw new Error("Canvas element not found");
-      }
-      if (!(canvas instanceof HTMLCanvasElement)) {
-        throw new Error("Element is not a canvas");
-      }
-
-      const ctx = canvas.getContext("2d");
-      if (!ctx) {
-        throw new Error("Failed to get canvas context");
-      }
-
-      canvas.style.border = "1px solid black";
-      canvas.style.backgroundColor = "lightgray";
-      this.canvas = canvas;
-      this.ctx = ctx;
-    }
+  if (!(canvas instanceof HTMLCanvasElement)) {
+    throw new Error("Element is not a canvas");
   }
-  const G = new GameCanvas();
-  export const canvas = G.canvas;
-  export const ctx = G.ctx;
+
+  const ctx = canvas.getContext("2d");
+  if (!ctx) {
+    throw new Error("Failed to get canvas context");
+  }
+
+  canvas.style.border = "1px solid black";
+  canvas.style.backgroundColor = "lightgray";
+
+  return { canvas, ctx };
+}
