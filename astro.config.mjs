62218d5ebb4fefa01ba9b390aa653eb5523b9bb9
TheMysteryz <themysteryz@users.noreply.github.com>
2025-06-13 16:01:41 +0200

    feat: add llms txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import conf from "./config.json";
+import starlightLlmsTxt from "starlight-llms-txt";
 
 // https://astro.build/config
 export default defineConfig({
@@ -44,6 +45,7 @@ export default defineConfig({
           autogenerate: { directory: "documentation" },
         },
       ],
+      plugins: [starlightLlmsTxt()],
     }),
   ],
 });
